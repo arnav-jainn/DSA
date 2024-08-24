9819b3dad7390382448e00e4bdd5913e240f235e
Arnav jain <arnavjainn02@gmail.com>
2024-08-24 20:45:54 +0530

    change

--- 2_Data Structures/Binary tree/prac.cpp
+++ 2_Data Structures/Binary tree/prac.cpp
@@ -2,19 +2,7 @@
 #include<queue>
 using namespace std;
 
-class Node{
-    public:
-    int data;
-    Node*left;
-    Node*right;
 
-    Node(int data){
-        this->data=data;
-        this->left=NULL;
-        this->right=NULL;
-
-    }
-};
 
 Node*createNode(){
     cout<<"enter node value: ";
